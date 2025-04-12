a5040e7e4bdc483d05ec2f4d9f3542408b8f05ad
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2025-04-12 13:56:21 +0530

    Update

--- 01_MY_ML_LIBRARY (FINAL DEPLOYED ON PYPI )/my_ml_library/__init__.py
+++ 01_MY_ML_LIBRARY (FINAL DEPLOYED ON PYPI )/my_ml_library/__init__.py
@@ -1,4 +1,3 @@
-from .models import SimpleLinearRegression, MultipleLinearRegression, LogisticRegression
 from .models import SimpleLinearRegression, MultipleLinearRegression, LogisticRegression, RidgeRegression, LassoRegression
 from .metrics import mean_squared_error, mean_absolute_error, root_mean_squared_error, r2_score, adjusted_r2_score
-from .optimizers import GradientDescent, StochasticGradientDescent
+from .optimizers import GradientDescent, StochasticGradientDescent  
